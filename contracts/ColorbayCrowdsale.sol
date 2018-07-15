3a63614cd7d4f834fcf73cbe4cd69ce50a740a10
Simon <vsiryxm@qq.com>
2018-07-15 12:18:34 +0800

    修改

--- contracts/ColorbayCrowdsale.sol
+++ contracts/ColorbayCrowdsale.sol
@@ -1,25 +1,14 @@
 pragma solidity ^0.4.23;
 
-import 'openzeppelin-solidity/contracts/crowdsale/validation/TimedCrowdsale.sol'; //SafeMath、Crowdsale
+import './Colorbay.sol';
+import 'openzeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol';
+import 'openzeppelin-solidity/contracts/crowdsale/validation/TimedCrowdsale.sol';
 
 
-contract ColorbayCrowdsale is TimedCrowdsale {
-    
-    constructor(uint256 _openingTime, uint256 _closingTime, uint256 _rate, address _wallet, ERC20 _token) public {
-        require(_openingTime >= block.timestamp);
-        require(_closingTime >= _openingTime);
-
-        require(_rate > 0);
-        require(_wallet != address(0));
-        require(_token != address(0));
-
-        openingTime = _openingTime;
-        closingTime = _closingTime;
-
-        rate = _rate;
-        wallet = _wallet;
-        token = _token;
+contract ColorbayCrowdsale is TimedCrowdsale, MintedCrowdsale {
+    constructor(uint256 _openingTime, uint256 _closingTime, uint256 _rate, address _wallet, MintableToken _token) public 
+    Crowdsale(_rate, _wallet, _token)
+    TimedCrowdsale(_openingTime, _closingTime) {
+        
     }
-
-
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,25 @@
-var Colorbay = artifacts.require("./Colorbay.sol");
 
+const ColorbayCrowdsale = artifacts.require('./ColorbayCrowdsale.sol');
+const Colorbay = artifacts.require("./Colorbay.sol");
 
-module.exports = function(deployer) {
-    //uint256 initialSupply, string tokenName, string tokenSymbol
-    deployer.deploy(Colorbay, '1000000000', 'Colorbay', 'CLB'); //注意，如果合约构造函数需要传递参数，从第二个位置开始增加参数 
+module.exports = function(deployer, network, accounts) {
+    const openingTime = web3.eth.getBlock('latest').timestamp + 2; //部署合约后的2秒后开始
+    const closingTime = openingTime + 86400 * 30; // 30天
+    const rate = new web3.BigNumber(1000);
+    const wallet = accounts[1];
+
+    return deployer
+        .then(() => {
+            return deployer.deploy(Colorbay, '1000000000', 'Colorbay', 'CLB'); //注意，如果合约构造函数需要传递参数，从第二个位置开始增加参数 
+        })
+        .then(() => {
+            return deployer.deploy(
+                ColorbayCrowdsale,
+                openingTime,
+                closingTime,
+                rate,
+                wallet,
+                Colorbay.address
+            );
+        });
 };
