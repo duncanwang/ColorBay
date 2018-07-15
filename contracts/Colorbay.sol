926a69b063b64b9801610c7cf82626a517d30a2e
Simon <vsiryxm@qq.com>
2018-07-15 09:56:13 +0800

    更新合约

--- contracts/Colorbay.sol
+++ contracts/Colorbay.sol
@@ -10,11 +10,20 @@ contract Colorbay is PausableToken,MintableToken,BurnableToken {
     uint256 public decimals = 18;
     
     //"1000000000","Colorbay","CLB"
-    constructor(uint256 initialSupply, string tokenName, string tokenSymbol) public {
-        totalSupply_ = initialSupply * 10 ** uint256(decimals);
+    constructor(uint256 _initialSupply, string _tokenName, string _tokenSymbol) public {
+        totalSupply_ = _initialSupply * 10 ** uint256(decimals);
         balances[msg.sender] = totalSupply_;
-        name = tokenName;
-        symbol = tokenSymbol;
+        name = _tokenName;
+        symbol = _tokenSymbol;
+    }
+
+    /**
+     * @dev Update the symbol.
+     * @param _tokenSymbol The symbol name.
+     */
+    function setSymbol(_tokenSymbol) public onlyOwner returns (bool) {
+        symbol = _tokenSymbol;
+        return true;
     }
 
 }
