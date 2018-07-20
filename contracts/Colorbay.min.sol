e07978adc9b101d7fb18e90155a221426694762b
Simon <vsiryxm@qq.com>
2018-07-20 17:44:31 +0800

    修改基础合约

--- contracts/Colorbay.min.sol
+++ contracts/Colorbay.min.sol
@@ -223,7 +223,6 @@ contract BasicToken is ERC20Basic {
 
 /**
  * @title Standard ERC20 token
- *
  * @dev Implementation of the basic standard token.
  * @dev https://github.com/ethereum/EIPs/issues/20
  * @dev Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
