7cc940731d4db4eec2623fc5bada09d2d5aaa5a2
Simon <vsiryxm@qq.com>
2018-08-01 21:02:07 +0800

    去除增发燃烧之后

--- contracts/Colorbay.min.sol
+++ contracts/Colorbay.min.sol
@@ -345,6 +345,7 @@ contract FrozenableToken is Ownable
     }
 
     function freezeAccount(address _to, bool _freeze) public onlyOwner {
+        require(_to != address(0));
         frozenAccount[_to] = _freeze;
         emit FrozenFunds(_to, _freeze);
     }
