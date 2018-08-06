8cee5ad3c946b5ad18075648c3245fe281aa1bd5
duncanwang <wangdenghui2005@sina.com>
2018-08-06 15:58:32 +0800

    增加乘，除的库函数
    增加乘，除的库函数

--- contracts/Colorbay.min.sol
+++ contracts/Colorbay.min.sol
@@ -6,6 +6,31 @@ pragma solidity ^0.4.23;
  */
 library SafeMath 
 {
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 _a, uint256 _b) internal pure returns (uint256 c) {
+    // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
+    // benefit is lost if 'b' is also tested.
+    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+    if (_a == 0) {
+      return 0;
+    }
+
+    c = _a * _b;
+    assert(c / _a == _b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 _a, uint256 _b) internal pure returns (uint256) {
+    // assert(_b > 0); // Solidity automatically throws when dividing by 0
+    // uint256 c = _a / _b;
+    // assert(_a == _b * c + _a % _b); // There is no case in which this doesn't hold
+    return _a / _b;
+  }
 
   /**
     * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
