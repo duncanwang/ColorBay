c492dcb98cc22a9044d07f36bfda75c7c5a62ce2
Simon <vsiryxm@qq.com>
2018-08-10 18:00:23 +0800

    删除renounceOwnership

--- contracts/Colorbay.min.sol
+++ contracts/Colorbay.min.sol
@@ -63,7 +63,6 @@ contract Ownable
 {
   address public owner;
 
-  event OwnershipRenounced(address indexed previousOwner);
   event OwnershipTransferred(
     address indexed previousOwner,
     address indexed newOwner
